b513dbde0711aeec064b2eb9b84662bf6998d4b8
sugoiJG <jasongolliet@icloud.com>
2023-05-31 09:50:05 -0700

    added api to Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-
-const axios = require('axios');
-
-const options = {
-  method: 'GET',
-  url: 'https://just-translated.p.rapidapi.com/',
-  params: {
-    lang: 'fr',
-    text: 'Hello, how are you?'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'f977b620c7msh7edc35cfaccd8b0p1c601fjsnb36e413da173',
-    'X-RapidAPI-Host': 'just-translated.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import InputForm from '../InputForm';
 
 const Home = () => {
-  return (
-    <div>Translator</div>
-  )
-}
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
 
-export default Home
+  const makeTranslationRequest = async () => {
+    axios
+    const options = {
+      method: 'GET',
+      url: 'https://just-translated.p.rapidapi.com/',
+      params: {
+        lang: 'en-jp',
+        text: inputText
+      },
+      headers: {
+        'X-RapidAPI-Key': 'f977b620c7msh7edc35cfaccd8b0p1c601fjsnb36e413da173',
+        'X-RapidAPI-Host': 'just-translated.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = axios.request(options);
+      setTranslatedText(response.data.translation);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  useEffect(() => {
+    makeTranslationRequest();
+  }, [inputText]);
+
+  return (
+    <div>
+      <input className="text-black m-6"
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="Enter text to translate"
+      />
+      <p>Translated Text: {translatedText}</p>
+    </div>
+  );
+};
+
+export default Home;
