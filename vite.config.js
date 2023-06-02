a253b3a3412ecd286986d817ab79fdc309a6966f
sugoiJG <jasongolliet@icloud.com>
2023-06-02 10:14:49 -0700

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/sugoiJG/learn-japanese-app",
+  base: "/",
   plugins: [react()],
 })
