17ac809cf0a0e04f3707e677d48611129264810c
Jason Golliet <jgolliet@LABA.PSWAS.LOCAL>
2023-06-12 13:46:01 -0700

    server fix

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import { fileURLToPath } from 'url'
 import express from 'express'
 import path from 'path'
 
+const app = express()
+const port = 3000
+
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
 
-const express = require('express')
-const app = express()
-const port = 3000
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
